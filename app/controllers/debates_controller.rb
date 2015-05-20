718bdb5b68ef6a00ee7f17461a689b18dca25113
wannamit <wannamit@gmail.com>
2015-05-20 12:04:34 +0545

    Define model relation between user debate and comment.

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -14,6 +14,7 @@ class DebatesController < ApplicationController
   # GET /debates/1.json
   def show
     @debate = Debate.find(params[:id])
+    @comments = @debate.comments
 
     respond_to do |format|
       format.html # show.html.erb
