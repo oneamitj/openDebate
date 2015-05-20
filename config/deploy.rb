c479135b2214f9e46c3b88d95bfbd2b698f27156
Kaji Bikash <kajisaap@gmail.com>
2015-05-20 11:56:50 +0545

    Production deployment should ask which branch to deploy with sensible default ...current branch you are in

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,10 @@ lock '3.4.0'
 set :application, 'sneha'
 set :repo_url, 'git@github.com:wannamit/openDebate.git'
 
+# Default branch is :master
+set :branch, ENV['BRANCH'] || proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, ENV['BRANCH'] || proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call if %w{production}.include?(ARGV.first)
+
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, "/home/deploy/#{fetch(:application)}"
 
