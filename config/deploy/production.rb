13a8cf62b7a8b0a3452a6f04e93393f7b358c3de
Kaji Bikash <kajisaap@gmail.com>
2015-05-20 12:03:20 +0545

    extra quote removed in banner msg

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 role :web, '184.72.2.117'
 role :app, '184.72.2.117'
 
-puts "\e[41m \e[1;37m #{fetch(:branch).upcase} in #{fetch(:rails_env).upcase} \e[0m \e[0m"]]]]"
+puts "\e[41m \e[1;37m #{fetch(:branch).upcase} in #{fetch(:rails_env).upcase} \e[0m \e[0m]]]]"
