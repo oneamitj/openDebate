e678cf5bf24035de30b9d6be66b6c17eabf6a33b
Sneha <elrosa.sneha@gmail.com>
2015-05-20 15:43:46 +0545

    add UI

--- app/assets/javascripts/debates.js
+++ app/assets/javascripts/debates.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready', function(){
+  $('.debate').mouseenter(function(){
+        var $meta = $(this).children('.meta');
+        var $edit = $($meta).children('.hover_show');
+        $edit.removeClass('hidden');
+    });
+
+    $('.debate').mouseleave(function(){
+        var $meta = $(this).children('.meta');
+        var $edit = $($meta).children('.hover_show');
+        $edit.addClass('hidden');
+    });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150520060228) do
+ActiveRecord::Schema.define(:version => 20150520072951) do
 
   create_table "comments", :force => true do |t|
     t.text     "description"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20150520060228) do
     t.integer  "debate_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "tag"
   end
 
   create_table "debates", :force => true do |t|
