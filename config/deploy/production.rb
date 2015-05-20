1797f70260b20c464ba2b5ec2e978d0d93b1df87
Kaji Bikash <kajisaap@gmail.com>
2015-05-20 12:06:29 +0545

    setting some convinience variable and fixing banner msg once again

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
+set :rails_env, (fetch(:rails_env) || fetch(:stage))
+
 role :web, '184.72.2.117'
 role :app, '184.72.2.117'
 
-puts "\e[41m \e[1;37m #{fetch(:branch).upcase} in #{fetch(:rails_env).upcase} \e[0m \e[0m]]]]"
+puts "\e[41m \e[1;37m #{fetch(:branch).upcase} in #{fetch(:rails_env).upcase} \e[0m \e[0m"
